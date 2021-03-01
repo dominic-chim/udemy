af2891906e75af710248602b5673c680b4e99eab
jimlaimun <li_man@live.co.uk>
2021-03-01 14:14:48 +0000

    react  component seperation

--- React/workbook/src/components/Persons/Person/Person.js
+++ React/workbook/src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css'
-const persons =(props)=>{
 
-    return(
-        <div className = {classes.Persons}>
-            <p onClick={props.click}> My name is {props.name}, I am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type='text' onChange={props.changed} value={props.name}/>
-        </div>
-    )
+class Person extends Component {
+    render(){
+        console.log("'[Person.js] rendering...");
+        return(
+            <div className = {classes.Person}>
+                <p onClick={this.props.click}> My name is {this.props.name}, I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type='text' onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        )
+    }
 }
 
-export default persons
+export default Person
--- React/workbook/src/containers/App.js
+++ React/workbook/src/containers/App.js
@@ -4,6 +4,12 @@ import Persons from '../components/Persons/Persons';
 import classes from './App.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log("'[App.js] constructor")
+  }
+
   state = {
     persons:[
       {id:'123',name:'david',age:21},
@@ -13,6 +19,15 @@ class App extends Component {
     ],
     otherState:'some other value',
     showPersons:false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("'[App.js] getDerivedStateFromProps",props);
+    return state;
+  }
+
+  componentDidMount(){
+    console.log("'[App.js] componentDidMount");
   }
 
   handleChangeHandler =(event,index) =>{
@@ -44,7 +59,7 @@ class App extends Component {
   }
 
   render() {
-
+    console.log("'[App.js] render");
     let persons = null;
     
     
